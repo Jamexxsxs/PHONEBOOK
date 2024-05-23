3fbef267ca5cdd7658c27a584b4b5b3e1d2903f3
Jomary Coquilla <jomarycoquilla465@gmail.com>
2024-05-23 11:14:28 +0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PhoneBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Denise Valerie\source\repos\PhoneBook\PhoneBook\App_Data\Phonebook.mdf;Integrated Security=True";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult addContact()
+        {
+            return View();
+        }
+        public ActionResult postAddContact()
+        {
+            var data = new List<object>();
+            string name = Request["name"];
+            string areacode =Request["areacode"];
+            string phonenumber = Request["phonenumber"];
+            string mobilenumber = Request["mobilenumber"];
+            int housenumber = int.Parse(Request["housenumber"]);
+            string street = Request["street"];
+            string city = Request["city"];
+            string province = Request["province"];
+            int zipcode = int.Parse(Request["zipcode"]);
+            string emailaddress = Request["emailaddress"];
+
+
+            using (var db = new SqlConnection(connStr))
+            {
+                db.Open();
+
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "INSERT INTO CONTACT (CON_NAME, CON_AREACODE, CON_PHONENUMBER, CON_MOBILENUMBER, CON_HOUSENUMBER, CON_STREET, CON_CITY, CON_PROVINCE, CON_ZIP, CON_EMAILADDRESS)" +
+                        " VALUES (@con_name, @con_areacode, @con_phonenumber, @con_mobilenumber, @con_housenumber, @con_street, @con_city, @con_province, @con_zip, @con_emailaddress)";
+                    cmd.Parameters.AddWithValue("@con_name", name);
+                    cmd.Parameters.AddWithValue("@con_areacode", areacode);
+                    cmd.Parameters.AddWithValue("@con_phonenumber", phonenumber);
+                    cmd.Parameters.AddWithValue("@con_mobilenumber", mobilenumber);
+                    cmd.Parameters.AddWithValue("@con_housenumber", housenumber);
+                    cmd.Parameters.AddWithValue("@con_street", street);
+                    cmd.Parameters.AddWithValue("@con_city", city);
+                    cmd.Parameters.AddWithValue("@con_province", province);
+                    cmd.Parameters.AddWithValue("@con_zip", zipcode);
+                    cmd.Parameters.AddWithValue("@con_emailaddress", emailaddress);
+
+                    try
+                    {
+                        var ctr = cmd.ExecuteNonQuery();
+
+                        if (ctr >= 1)
+                        {
+                            data.Add(new
+                            {
+                                mess = 1
+                            });
+                        }
+                    }
+                    catch(SqlException ex)
+                    {
+                        if (ex.Message.Contains("Violation of UNIQUE KEY constraint"))
+                        {
+                            data.Add(new
+                            {
+                                mess = 0
+                            });
+                        }
+                    }
+                }
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult viewAllContact()
+        {
+            var data = new List<Dictionary<string, object>>();
+
+            using (var db = new SqlConnection(connStr))
+            {
+                db.Open();
+
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT CON_NAME, CON_AREACODE, CON_PHONENUMBER, CON_MOBILENUMBER, CON_HOUSENUMBER, CON_STREET, CON_CITY, CON_PROVINCE, CON_ZIP, CON_EMAILADDRESS FROM CONTACT";
+                    
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var contact = new Dictionary<string, object>
+                            {
+                                { "CON_NAME", reader["CON_NAME"] },
+                                { "CON_AREACODE", reader["CON_AREACODE"] },
+                                { "CON_PHONENUMBER", reader["CON_PHONENUMBER"] },
+                                { "CON_MOBILENUMBER", reader["CON_MOBILENUMBER"] },
+                                { "CON_HOUSENUMBER", reader["CON_HOUSENUMBER"] },
+                                { "CON_STREET", reader["CON_STREET"] },
+                                { "CON_CITY", reader["CON_CITY"] },
+                                { "CON_PROVINCE", reader["CON_PROVINCE"] },
+                                { "CON_ZIP", reader["CON_ZIP"] },
+                                { "CON_EMAILADDRESS", reader["CON_EMAILADDRESS"] },
+                            };
+                            data.Add(contact);
+                        }
+                    }
+                }
+            }
+            ViewBag.Contacts = data;
+            return View();
+        }
+
+        public ActionResult postSearch()
+        {
+            var data = new List<Dictionary<string, object>>();
+            string search = Request["search"];
+
+            using (var db = new SqlConnection(connStr))
+            {
+                db.Open();
+
+                using (var cmd = db.CreateCommand())
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT CON_NAME, CON_AREACODE, CON_PHONENUMBER, CON_MOBILENUMBER, CON_HOUSENUMBER, CON_STREET, CON_CITY, CON_PROVINCE, CON_ZIP, CON_EMAILADDRESS FROM CONTACT " +
+                        " WHERE CON_PHONENUMBER LIKE @search";
+                    cmd.Parameters.AddWithValue("@search", "%" + search + "%");
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var contact = new Dictionary<string, object>
+                            {
+                                { "CON_NAME", reader["CON_NAME"] },
+                                { "CON_AREACODE", reader["CON_AREACODE"] },
+                                { "CON_PHONENUMBER", reader["CON_PHONENUMBER"] },
+                                { "CON_MOBILENUMBER", reader["CON_MOBILENUMBER"] },
+                                { "CON_HOUSENUMBER", reader["CON_HOUSENUMBER"] },
+                                { "CON_STREET", reader["CON_STREET"] },
+                                { "CON_CITY", reader["CON_CITY"] },
+                                { "CON_PROVINCE", reader["CON_PROVINCE"] },
+                                { "CON_ZIP", reader["CON_ZIP"] },
+                                { "CON_EMAILADDRESS", reader["CON_EMAILADDRESS"] }
+                            };
+                            data.Add(contact);
+                        }
+                    }
+
+                }
+            }
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ viewAllContact.js
@@ -0,0 +1,104 @@
+﻿$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        var searchValue = $("#search").val();
+
+        if (!(/^\d{7}$/.test(searchValue))) {
+            alert("Please enter a valid 7-digit phone number.");
+            return; 
+        }
+
+        $.post('../Home/postSearch',
+            {
+                search: searchValue
+            },
+            function (data) {
+                $('table tbody').empty();
+
+                if (data.length === 0) {
+                    $('table tbody').append('<tr><td colspan="10">No results found.</td></tr>');
+                }
+                else {
+                    data.forEach(function (contact) {
+                        var newRow = '<tr>' +
+                            '<td>' + contact["CON_NAME"] + '</td>' +
+                            '<td>' + contact["CON_AREACODE"] + '</td>' +
+                            '<td>' + contact["CON_PHONENUMBER"] + '</td>' +
+                            '<td>' + contact["CON_MOBILENUMBER"] + '</td>' +
+                            '<td>' + contact["CON_HOUSENUMBER"] + '</td>' +
+                            '<td>' + contact["CON_STREET"] + '</td>' +
+                            '<td>' + contact["CON_CITY"] + '</td>' +
+                            '<td>' + contact["CON_PROVINCE"] + '</td>' +
+                            '<td>' + contact["CON_ZIP"] + '</td>' +
+                            '<td>' + contact["CON_EMAILADDRESS"] + '</td>' +
+                            '</tr>';
+                        $('table tbody').append(newRow);
+                    })
+                }
+                $('#backBtn').show();
+            }
+        )
+    })
+
+    $("#backBtn").click(function () {
+        location.reload(); 
+    });
+
+    $("#submitContact").click(function () {
+
+        var name = $("#name").val().toUpperCase();
+        var areacode = $("#areacode").val().substr(0, 3);
+        var phonenumber = $("#phonenumber").val().substr(0, 7);
+        var mobilenumber = $("#mobilenumber").val().substr(0, 11);
+
+        if (areacode.length !== 3) {
+            alert('Area code must be 3 digits.');
+            return;
+        }
+
+        if (phonenumber.length !== 7) {
+            alert('Phone number must be 7 digits.');
+            return;
+        }
+
+        if (mobilenumber.length !== 11) {
+            alert('Mobile number must be 11 digits.');
+            return;
+        }
+
+        $.post('../Home/postAddContact',
+            {
+                name: name,
+                areacode: areacode,
+                phonenumber: phonenumber,
+                mobilenumber: mobilenumber,
+                housenumber: $("#housenumber").val(),
+                street: $("#street").val(),
+                city: $("#city").val(),
+                province: $("#province").val(),
+                zipcode: $("#zipcode").val(),
+                emailaddress: $("#emailaddress").val()
+            },
+            function (data) {
+                if (data[0].mess == 1) {
+                    var newRow = '<tr>' +
+                        '<td>' + $("#name").val().toUpperCase() + '</td>' +
+                        '<td>' + $("#areacode").val() + '</td>' +
+                        '<td>' + $("#phonenumber").val() + '</td>' +
+                        '<td>' + $("#mobilenumber").val() + '</td>' +
+                        '<td>' + $("#street").val() + '</td>' +
+                        '<td>' + $("#name").val() + '</td>' +
+                        '<td>' + $("#city").val() + '</td>' +
+                        '<td>' + $("#province").val() + '</td>' +
+                        '<td>' + $("#zipcode").val() + '</td>' +
+                        '<td>' + $("#emailaddress").val() + '</td>' +
+                        '</tr>';
+                    $('table tbody').append(newRow);
+                    $('#addProductModal').modal('hide');
+                }
+                else {
+                    alert('Phone Number Exist')
+                }
+            }
+        )
+    })
+})
